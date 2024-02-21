2c9ffecc2af687d7235fc1bde14288166ccc28f7
Abhay <abhaysharma.developer@gmail.com>
2024-02-21 11:00:46 +0530

    Horizontal Scroll

--- src/App.js
+++ src/App.js
@@ -6,10 +6,53 @@ import Navbar from "./components/Navbar/Navbar";
 import Skill from "./components/Skill/Skill";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import { useRef, useState } from "react";
 
 function App() {
+  const itemsRef = useRef(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const handleMouseDown = (e) => {
+    setIsMouseDown(true);
+    setStartX(e.pageX - itemsRef.current.offsetLeft);
+    setScrollLeft(itemsRef.current.scrollLeft);
+  };
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+  const handleMouseMove = (e) => {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - itemsRef.current.offsetLeft;
+    const walk = (x - startX) * 2; //adjust speed
+    itemsRef.current.scrollLeft = scrollLeft - walk;
+  };
+  const handleMouseLeave = () => {
+    setIsMouseDown(false);
+  };
+
+  function horizontalScroll(event) {
+    const delta = Math.max(
+      -1,
+      Math.min(1, event.nativeEvent.wheelDelta || -event.nativeEvent.detail)
+    );
+    event.currentTarget.scrollLeft -= delta * 100;
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      id="App"
+      ref={itemsRef}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      onWheel={horizontalScroll}
+    >
       <Navbar />
       <Home />
       <About />
